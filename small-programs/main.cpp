ca92c11ab1bb83e5ba3e2bef9ad15c4a083e5713
Jesus Orduna <orduna.jesus@gmail.com>
2014-09-25 16:00:13 -0500

    first commit

---
+++ small-programs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  small-programs
+//
+//  Created by Jesus Orduna on 9/25/14.
+//  Copyright (c) 2014 Brown University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
